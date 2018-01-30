6d701107e9428ff64a65b546b7327251fe59ea22
surbhi rawat <surbhirawat96@gmail.com>
2018-01-30 18:35:22 +0530

    validation

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,10 @@ $(document).ready(function(){
 var letters = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 $("#emailId").focusout(function() {
   if (!emailId.value.match(letters)) { // not email
-    emailId.classList.add('invalid');
-    error.innerHTML = 'Please enter a correct email.'
+     emailId.classList.add('invalid');
+     error.innerHTML = 'Please enter a correct email.'
   }
+  $("#error2").hide();
 });
 
 $("#emailId").focusin(function() {
@@ -23,6 +24,7 @@ $("#emailId").focusin(function() {
     this.classList.remove('invalid');
     error.innerHTML = "";
   }
+
 });
 
 //password
@@ -33,6 +35,7 @@ $("#pswrd").focusout(function() {
     pswrd.classList.add('invalid');
     wrong.innerHTML = 'Please enter a correct password.'
   }
+  $("#error3").hide();
 });
 
 $("#pswrd").focusin(function() {
@@ -48,12 +51,33 @@ $(function () {
          var password = $("#pswrd").val();
          var confirmPassword = $("#pswrd2").val();
          if (password != confirmPassword) {
-          wrong2.innerHTML = "Passwords do not match."
+          //wrong2.innerHTML = "Passwords do not match."
+          $("#wrong2").text( "password does not match" ).show();
           //alert("Passwords do not match.");
           return false;
          }
          return true;
-     });
+       });
+   });
+   $("#pswrd2").focusin(function(){
+     $("#wrong2").hide();
+   });
+
+//to hide the error msgs
+   $("#mobile-number").focusin(function(){
+     $("#error5").hide();
+   });
+   $("#address").focusin(function(){
+     $("#error6").hide();
+   });
+   $("#datepicker").focusin(function(){
+     $("#error7").hide();
+   });
+    /*.focus(function() {
+      		$('#wrong2').show();
+      	}).blur(function() {
+      		$('#wrong2').hide();
+      	});*/
 
 /*$('input[name=password]').keyup(function () {
     'use strict';
@@ -67,83 +91,66 @@ $(function () {
     }
 });*/
 
-});
+
 
 //for autocomplete places
-var autocomplete = new google.maps.places.Autocomplete($("#address")[0], {});
+    var autocomplete = new google.maps.places.Autocomplete($("#address")[0], {});
 
-            google.maps.event.addListener(autocomplete, 'place_changed', function() {
-                var place = autocomplete.getPlace();
-                console.log(place.address_components);
-            });
-//phoneNumber
-$("#mobile-number").intlTelInput();
-$(":input").inputmask();
-$("#mobile-number").inputmask({"mask": "(999) 999-9999"});
+              google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                  var place = autocomplete.getPlace();
+                  console.log(place.address_components);
+              });
+    //phoneNumber
+    $("#mobile-number").intlTelInput();
+    $(":input").inputmask();
+    $("#mobile-number").inputmask({"mask": "(999) 999-9999"});
 
+    //date Validation
+    $( function() {
+     $( "#datepicker" ).datepicker();
+    });
+
+ // After Form Submitted Validation
+     $("#btnSubmit").on('click',function(){
+
+      var myId = $("#emailId").val();
+      var myPassword = $("#pswrd").val();
+      var myPhone = $("#mobile-number").val();
+      var myAdd = $("#address").val();
+      var myDate = $("#datepicker").val();
+
+      if(myId ==  "" || myId ==  undefined )
+      {
+        $("#error2").text( "this field is required" ).show();
+        return false;
+      }
+      if(myPassword ==  undefined || myPassword =="")
+      {
+        $("#error3").text( "this field is required" ).show();
+        //alert("please fill the email");
+        return false;
+      }
+      if(myAdd ==  undefined || myAdd =="")
+      {
+        $("#error6").text( "this field is required" ).show();
+        //alert("please fill the email");
+        return false;
+      }
+      if(myPhone ==  undefined || myPhone =="")
+      {
+        $( "#error5").text( "this field is required" ).show();
+        //alert("please fill the email");
+        return false;
+      }
+      if(myDate ==  undefined || myDate =="")
+      {
+        $( "#error7").text( "this field is required" ).show();
+        //alert("please fill the email");
+        return false;
+      }
+
+    });
+
+     $("#btnSubmit").on('click',function(){
+     });
 });
-
-
-    //   $('#signUp')
-    //     .find('[name="phoneNumber"]')
-    //         .intlTelInput({
-    //             utilsScript: 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.7/js/utils.js',
-    //             autoPlaceholder: true,
-    //             preferredCountries: ['fr', 'us', 'gb']
-    //         });
-    //
-    // $('#signUp')
-    //     .formValidation({
-    //         framework: 'bootstrap',
-    //         icon: {
-    //             valid: 'glyphicon glyphicon-ok',
-    //             invalid: 'glyphicon glyphicon-remove',
-    //             validating: 'glyphicon glyphicon-refresh'
-    //         },
-    //         fields: {
-    //             phoneNumber: {
-    //                 validators: {
-    //                     callback: {
-    //                         message: 'The phone number is not valid',
-    //                         callback: function(value, validator, $field) {
-    //                             return value === '' || $field.intlTelInput('isValidNumber');
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     })
-    //     // Revalidate the number when changing the country
-    //     .on('click', '.country-list', function() {
-    //         $('#contactForm').formValidation('revalidateField', 'phoneNumber');
-    //     });
-
-
-//   $.validator.addMethod('strongPassword',function(value, element){
-//     return this.optional(element)
-//     || value.length >=12 && /\d/.test(value) && /[a-z]/i.test(value);
-//   },'password must contain atleast 12 char long, one number & one char\'.')
-//
-//   $("#signUp").validate({
-//     rules: {
-//       email: {
-//         required: true,
-//         email: true
-//       },
-//       password: {
-//         required: true,
-//         strongPassword: true
-//       },
-//         password2:{
-//           required: true,
-//           equalTo: "#password"
-//         }
-//       },
-//     messages: {
-//       email: {
-//         required: "enter an email address.",
-//         email: "please enter a valid email."
-//       }
-//     }
-//   });
-//
